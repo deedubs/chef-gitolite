22426ff4fd1624e68552b35fb547efd7458127bb
Gonzalo Rodríguez-Baltanás Díaz <siotopo@gmail.com>
2011-11-06 20:25:36 +0100

    Even better, remove any prior gitolite-source folder

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,6 +22,7 @@ require_recipe "git"
 bash 'install_gitolite' do
   cwd "/tmp"
   code <<-EOH
+    rm -rf gitolite-source
     git clone git://github.com/sitaramc/gitolite gitolite-source
     cd gitolite-source
     git checkout -t -f origin/pu
